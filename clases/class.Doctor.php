abba9fffc36d00b805033a83261451f78a285b43
Enrique De La Cruz <enriquedelacruz04@outlook.com>
2021-11-20 01:40:41 -0600

    doctor

---
+++ clases/class.Doctor.php
@@ -0,0 +1,88 @@
+<?php
+class Doctor
+{
+    //=========================  Objeto de conexion con la base de datos
+    public $db;
+
+    //========================= ID's
+    // public $ultimoId;
+    // public $id;
+
+    //========================= Variables de la DB 
+    public $cedula;
+    public $nombre;
+    public $edad;
+    public $sexo;
+    public $telefono;
+    public $especialidad;
+
+    //=========================  Datos de la tabla de la DB
+    private $tablaNombre = "doctor";
+    private $tablaId = "cedula";
+
+    //===========================================================
+    // Funciones 
+    //=========================================================== 
+
+    //========================= Guardar nuevo registro 
+    public function guardarDoctor()
+    {
+        $query = "INSERT INTO $this->tablaNombre
+			(cedula, nombre, edad, sexo, telefono, especialidad)
+
+			 VALUES 
+			 (
+			 '$this->cedula',
+			 '$this->nombre',
+			 '$this->edad',
+			 '$this->sexo',
+			 '$this->telefono'
+			 '$this->especialidad'
+			 )";
+
+        $result = $this->db->consulta($query);
+        $this->ultimoId = $this->db->id_ultimo();
+        return $result;
+    }
+
+    //========================= Modificar registro 
+    public function modificarDoctor()
+    {
+        $query = "UPDATE $this->tablaNombre SET 
+		idSucursales = '$this->idSucursal'
+
+		WHERE $this->tablaId = '$this->id'";
+
+        $result = $this->db->consulta($query);
+        return $result;
+    }
+
+    //========================= Consulta todos los registros
+    public function getAllDoctor()
+    {
+        $query = "SELECT * FROM $this->tablaNombre";
+        $result = $this->db->consulta($query);
+        $numResult = $this->db->num_rows($result);
+
+        if ($numResult > 0) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    //========================= Consulta 1 registro 
+    public function getOneDoctor($id)
+    {
+        $query = "SELECT * FROM $this->tablaNombre WHERE $this->tablaId = $id";
+        $result = $this->db->consulta($query);
+        $numResult = $this->db->num_rows($result);
+        $rowResult = $this->db->fetch_assoc($result);
+
+        if ($numResult > 0) {
+            return $rowResult;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ modulos/doctor/vi_doctor.php
@@ -0,0 +1,93 @@
+<?php
+require_once("../../clases/conexcion.php");
+require_once("../../clases/class.Sesion.php");
+require_once("../../clases/class.Funciones.php");
+require_once("../../clases/class.Doctor.php");
+
+//========================= Funciones
+$db = new MySQL();
+$se = new Sesion();
+$fun = new Funciones();
+$doctor = new Doctor();
+$doctor->db = $db;
+
+//========================= Sesion 
+if (!isset($_SESSION['se_SAS'])) {
+    header("Location: ../login.php");
+    exit;
+}
+
+//========================= Rutas
+$rutaViDoctor = "modulos/doctor/vi_doctor.php";
+$rutaFaDoctor = "modulos/doctor/fa_doctor.php";
+$rutaGaDoctor = "modulos/doctor/ga_doctor.php";
+?>
+
+<!-- //=========================================================== -->
+<!-- // Vista de tabla  -->
+<!-- //===========================================================  -->
+
+<div class="card th-card-titulo">
+    <div class="card-header">
+        <h5 class="card-title">DOCTORES</h5>
+        <div class="card-botones">
+        </div>
+    </div>
+</div>
+
+<div class="card th-card-table">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table id="zeroConfig" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th class="text-center">CEDULA</th>
+                        <th class="text-center">NOMBRE</th>
+                        <th class="text-center">EDAD</th>
+                        <th class="text-center">SEXO</th>
+                        <th class="text-center">TELEFONO</th>
+                        <th class="text-center">ESPECIALIDAD</th>
+                        <th class="text-center">ACCIONES</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php
+
+                    //========================= Consulta
+                    $consultaDoctor = $doctor->getAllDoctor();
+                    while ($rowDoctor = $db->fetch_assoc($consultaDoctor)) {
+
+                        //========================= Variables de la vista
+                        $cedula = $rowDoctor['cedula'];
+                        $nombre = $rowDoctor['nombre'];
+                        $edad = $rowDoctor['edad'];
+                        $sexo = $rowDoctor['sexo'];
+                        $telefono = $rowDoctor['telefono'];
+                        $especialidad = $rowDoctor['especialidad'];
+                    ?>
+                        <tr>
+                            <td align="center"><?= $cedula ?></td>
+                            <td align="center"><?= $nombre ?></td>
+                            <td align="center"><?= $edad ?></td>
+                            <td align="center"><?= $sexo ?></td>
+                            <td align="center"><?= $telefono ?></td>
+                            <td align="center"><?= $especialidad ?></td>
+                            <td align="center">
+
+                            <!-- Editar -->
+                            <button onClick="aparecermodulos('<?= $rutaFaDoctor . "?numero=$numero" ?>','main');" type="button" class="btn btn-outline-info" title="EDITAR"><i class="fas fa-pencil-alt"></i></button>
+
+                            <!-- Eliminar -->
+                            <button onClick="BorrarDatos('<?= $numero; ?>','numero','doctor','n','<?= $rutaViDoctor ?>','main');" type="button" class="btn btn-outline-danger" title="ELIMINAR"><i class="fas fa-trash-alt"></i></button>
+
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
